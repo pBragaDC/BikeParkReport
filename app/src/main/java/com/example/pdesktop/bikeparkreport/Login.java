87c23bc5f4ebf64090ef7ff8b81fa303f1cc1577
Pedro <br478@yahoo.com>
2015-11-06 14:32:21 -0500

    Initial Commit
    Splash Screen code and Layout done, Login screen layout done still need
    code

---
+++ app/src/main/java/com/example/pdesktop/bikeparkreport/Login.java
@@ -0,0 +1,14 @@
+package com.example.pdesktop.bikeparkreport;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class Login extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+
+}
---
+++ app/src/main/java/com/example/pdesktop/bikeparkreport/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.pdesktop.bikeparkreport;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends ActionBarActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//remove title
+        //remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //set content view
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, Login.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
